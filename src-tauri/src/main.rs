09d0998378e66a47832f7a561510e92704c391d9
ssamyne <klitsanac@hotmail.com>
2024-06-18 14:43:14 +0700

    ⚰️ remove deadcode

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::thread::sleep;
-
 use tauri::{ Manager };
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
